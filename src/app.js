166511f4af63e52702299336e75581f9acdfdef7
Nuaiman Ashiq <ashiqnuaiman@gmail.com>
2024-12-04 15:40:04 +0600

    commit

--- src/app.js
+++ src/app.js
@@ -4,11 +4,11 @@ import cookieParser from "cookie-parser"
 
 const app = express();
 // --
-app.use(cookieParser())
 app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true, }))
 app.use(express.json({ limit: "16Kb" }))
 app.use(express.urlencoded({ extended: true, limit: "16Kb" }))
 app.use(express.static("public"))
+app.use(cookieParser())
 // --
 import healthCheckRouter from "./routes/healthcheck.routes.js"
 // --
