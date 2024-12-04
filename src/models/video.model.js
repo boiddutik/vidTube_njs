90358083c392da22dfe68587d3119ac61206e1a7
Nuaiman Ashiq <ashiqnuaiman@gmail.com>
2024-12-04 11:42:35 +0600

    commit

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,52 +1,47 @@
-/*
-id string pk
-owner ObjectId users
-videoFile string
-thumbnail string
-title string
-description string
-duration number
-views number
-isPublished boolean
-createdAt Date
-updatedAt Date
-*/
+import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
-import mongoose, { Schema } from "mongoose"
+const videoSchema = new Schema(
+    {
+        videoFile: {
+            type: String, //cloudinary url
+            required: true
+        },
+        thumbnail: {
+            type: String, //cloudinary url
+            required: true
+        },
+        title: {
+            type: String,
+            required: true
+        },
+        description: {
+            type: String,
+            required: true
+        },
+        duration: {
+            type: Number,
+            required: true
+        },
+        views: {
+            type: Number,
+            default: 0
+        },
+        isPublished: {
+            type: Boolean,
+            default: true
+        },
+        owner: {
+            type: Schema.Types.ObjectId,
+            ref: "User"
+        }
 
-const videoSchema = new Schema({
-    owner: {
-        type: Schema.Types.ObjectId,
-        ref: "User",
     },
-    videoFile: {
-        type: String, // s3, cloudinary (later)
-        required: true,
-    },
-    thumbnail: {
-        type: String, // s3, cloudinary (later)
-        required: true,
-    },
-    title: {
-        type: String,
-        required: true,
-    },
-    description: {
-        type: String,
-    },
-    views: {
-        type: Number,
-        default: 0,
-    },
-    duration: {
-        type: Number,
-    },
-    isPublished: {
-        type: Boolean,
-        default: true,
-    },
-},
-    { timestamps: true }
+    {
+        timestamps: true
+    }
 )
 
-export const Video = mongoose.model("Video", userSchema)
+videoSchema.plugin(mongooseAggregatePaginate)
+
+export const Video = mongoose.model("Video", videoSchema)
