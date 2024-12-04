c5b391e2ca5bb9c0496cbbff8f1efe8ecb42f619
Nuaiman Ashiq <ashiqnuaiman@gmail.com>
2024-12-04 15:25:44 +0600

    commit

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser"
 
 const app = express();
 // --
+app.use(cookieParser())
 app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true, }))
 app.use(express.json({ limit: "16Kb" }))
 app.use(express.urlencoded({ extended: true, limit: "16Kb" }))
