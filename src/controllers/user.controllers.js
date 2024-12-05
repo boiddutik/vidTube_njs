8be57e2b8202b8b3accd6539e7b1a6f6530c4135
Nuaiman Ashiq <ashiqnuaiman@gmail.com>
2024-12-05 08:09:35 +0600

    commit

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -6,38 +6,36 @@ import { User } from "../models/user.model.js"
 
 const registerUser = asyncHandler(async (req, res) => {
     const { userName, fullName, email, password } = req.body;
-
     if (
         [userName, fullName, email, password].some((field) => field?.trim() === "")
     ) {
         throw new ApiError(400, "All fields are required")
     }
-
     const existingUser = await User.findOne({
         $or: [{ userName }, { email }]
     })
-
     if (existingUser) {
         throw new ApiError(409, "User already exists")
     }
-
     const avatar = req.files?.avatar?.[0]?.path;
     const cover = req.files?.cover?.[0]?.path;
-
     if (!avatar) {
         throw new ApiError(400, "Avatar is missing")
     }
-
-    const createdUser = await User.create({
-        userName, fullName, email, password, avatar, cover
-    })
-
+    const createdUser = await User.create(
+        {
+            userName: userName,
+            fullName,
+            avatar: avatar,
+            cover: cover || "",
+            email,
+            password,
+        },
+    )
     const user = await User.findById(createdUser._id).select("-password -refreshToken")
-
     if (!user) {
         throw new ApiError(500, "Something went wrong while registering user")
     }
-
     return res.status(201).json(new ApiResponse(201, user, "User registration complete."))
 })
 
